e4d213d46d6cf4eb1fb77574b3e52a18bb11c750
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 22:00:26 +0530

    updated index

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -5,10 +5,10 @@ function Real() {
     <div className="real section w-full">
       <div className="cont h-[400vh] bg-green-600 relative">
         <div className="slides w-full h-[100vh] sticky top-0 left-0 flex ">
-            <div className="slide w-full h-screen"></div>
-            <div className="slide w-full h-screen"></div>
-            <div className="slide w-full h-screen"></div>
-            <div className="slide w-full h-screen"></div>
+            <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
+            <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
+            <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
+            <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
         </div>
       </div>
     </div>
