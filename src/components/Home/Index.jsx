c3fa3ce6b9e4e6b6f0b5f2cce60d1cd46825c098
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 20:17:20 +0530

    updated index

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -1,26 +1,9 @@
 import video from '../../assets/video/1ENIoa5sjq.mp4'
-import img1 from '../../assets/images/img1.png';
-import img2 from '../../assets/images/img2.png';
-import img3 from '../../assets/images/img3.png';
-import img4 from '../../assets/images/img4.png';
-import img5 from '../../assets/images/img5.png';
-import img6 from '../../assets/images/img6.png';
-import img7 from '../../assets/images/img7.png';
-
-
+import Row from '../Row'
 
 
 function Home() {
-    const items = [
-        { text: "useful", image: img7 },
-        { text: "intuitive", image: img2 },     
-        { text: "empathetic", image: img3 },     
-        { text: "useful", image: img4 },     
-        { text: "empathetic", image: img5 },
-        { text: "intuitive", image: img2 },     
-        { text: "empathetic", image: img6 },
-        { text: "useful", image: img1 },               
-    ]
+   
     
   return (
     <div className=" section w-full h-screen relative ">
@@ -60,72 +43,11 @@ function Home() {
             <div 
                 className='absolute scale-[1.3] top-1/2 left-1/2 
                 -translate-x-1/2 -translate-y-1/2 w-[90%]'
-            >
-                {/* 1st row */}
-                <div className='row -translate-x-1/2 w-full flex items-center gap-8 whitespace-nowrap'>
-                    {items.map((item, index) => {
-                        return (
-                            <div key={index} className='elem flex items-center gap-8'>
-                                <h1  className='font-[SansitaBold] text-6xl'>{item.text}</h1>
-                                <div className='imgdiv w-[2rem] h-[2rem]'>
-                                    <img 
-                                        className=''
-                                        src={item.image} 
-                                    />
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-                {/* 2nd row */}
-                <div className='row -translate-x-1/3 w-full flex items-center gap-8 whitespace-nowrap'>
-                    {items.map((item, index) => {
-                        return (
-                            <div key={index} className='elem flex items-center gap-8'>
-                                <h1  className='font-[SansitaBold] text-6xl'>{item.text}</h1>
-                                <div className='imgdiv w-[2rem] h-[2rem]'>
-                                    <img 
-                                        className=''
-                                        src={item.image} 
-                                    />
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-                {/* 3nd row */}
-                <div className='row -translate-x-2/3 w-full  flex items-center gap-8 whitespace-nowrap'>
-                    {items.map((item, index) => {
-                        return (
-                            <div key={index} className='elem flex items-center gap-8'>
-                                <h1  className='font-[SansitaBold] text-6xl'>{item.text}</h1>
-                                <div className='imgdiv w-[2rem] h-[2rem]'>
-                                    <img 
-                                        className=''
-                                        src={item.image} 
-                                    />
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-                {/* 4nd row */}
-                <div className='row -translate-x-1/4 w-full  flex items-center gap-8 whitespace-nowrap
-                '>
-                    {items.map((item, index) => {
-                        return (
-                            <div key={index} className='elem flex items-center gap-8'>
-                                <h1  className='font-[SansitaBold] text-6xl'>{item.text}</h1>
-                                <div className='imgdiv w-[2rem] h-[2rem]'>
-                                    <img 
-                                        className=''
-                                        src={item.image} 
-                                    />
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>             
+            >                
+                <Row translateClass="-translate-x-1/2"/>
+                <Row translateClass="-translate-x-2/3"/>
+                <Row translateClass="-translate-x-1/4" />
+                <Row translateClass="-translate-x-1/3"/>
             </div>
         </div>
     </div>
