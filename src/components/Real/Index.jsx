92673640c185037bc3d7d847d832bf7ebd273632
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 22:00:36 +0530

    updated

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -6,7 +6,7 @@ function Real() {
       <div className="cont h-[400vh] bg-green-600 relative">
         <div className="slides w-full h-[100vh] sticky top-0 left-0 flex ">
             <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
-            <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
+            <div className="slide w-full h-screen flex-shrink-0 bg-yellow-500"></div>
             <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
             <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
         </div>
