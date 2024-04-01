e9ad8aa907586d287ccd62ed54894a0f700c6ef3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 22:17:26 +0530

    updated

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -3,11 +3,18 @@
 function Real() {
   return (
     <div className="real section w-full">
-      <div className="cont h-[400vh] bg-green-600 relative">
+      <div className="cont h-[400vh]  relative">
         <div className="slides w-full h-[100vh] sticky top-0 left-0 flex ">
-            <div className="slide w-full h-screen flex-shrink-0 bg-red-500">
-                <h1 className="font-[SansitaReg]">Real Talk,
-Real Impact</h1>
+            <div className="slide w-full flex items-center justify-center h-screen flex-shrink-0 ">
+                <div className="text font-[SansitaReg] text-[15vh] leading-[18vh]">
+                    <h1 className="">
+                        Real Talk,    
+                    </h1>
+                    <h1 className="">
+                        Real Impact
+                    </h1>
+                </div>
+                <div className=""></div>
             </div>
             <div className="slide w-full h-screen flex-shrink-0 bg-yellow-500"></div>
             <div className="slide w-full h-screen flex-shrink-0 bg-green-500"></div>
