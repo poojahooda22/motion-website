6a365171d81992bc58a7d14e7ec1c9159945bd44
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 21:56:27 +0530

    updated index

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -5,7 +5,7 @@ function Real() {
     <div className="real section w-full">
       <div className="cont h-[400vh] bg-green-600">
         <div className="slides w-full h-[100vh] bg-purple-600 ">
-            <div></div>
+            <div className="slide"></div>
 
         </div>
       </div>
