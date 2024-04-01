5fb0dffa85759fcc69a4433988622afe16d7c733
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 13:00:35 +0530

    updated index

--- src/components/Craft/Index.jsx
+++ src/components/Craft/Index.jsx
@@ -4,7 +4,7 @@ function Cart() {
   return (
     <div className="craft w-full flex gap-10 px-10">
         <div className="ltext w-1/3 py-10 bg-red-300">
-            <p>
+            <p className="font-[Sansita] font-medium ">
                 Significo is a leading health software creator founded on the belief
                 that technology can transform healthcare to put people first.
                 Whether they are employees, members, customers, or clients, we
