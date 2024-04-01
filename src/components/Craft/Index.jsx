766cb9895cd4811c2c282413fe9673721143c196
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 20:17:14 +0530

    updated index

--- src/components/Craft/Index.jsx
+++ src/components/Craft/Index.jsx
@@ -24,10 +24,26 @@ function Cart() {
         </div>
 
 
-        <div className="cards w-1/2 bg-red-700 ">
-            <p className="text-3xl">
-                
-            </p>
+        <div className="cards w-1/2 ">
+            <div className="card w-2/3 flex bg-[--black] py-10 px-16 text-[--dcyan]">
+                <div className="text-[--dcyan] font-[Sansita]  leftdata">
+                    <h1 className="text- font-semibold">Driven by Purpose</h1>
+                    <p className="text-[1rem]">We believe technology can dramatically
+                        improve the experience of managing health,
+                        and when crafted with empathy, intention,
+                        and expertise, impact lives at scale.
+                    </p>
+
+                </div>
+                <div className="rightdata">
+                    <svg width="100%" height="100%" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path opacity="0.2" d="M45.9998 10H31.9998L19.7271 26L31.9998 56L59.9998 26L45.9998 10Z" fill="currentColor"></path>
+                    <path d="M18 10H46L60 26L32 56L4 26L18 10Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M44.2726 26L31.9998 56L19.7271 26L31.9998 10L44.2726 26Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                    <path d="M4 26H60" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+                    </svg>
+                </div>
+            </div>
         </div>
     </div>
   )
