c3637058e515229bbeefb09ebe3b5a7fc0079dde
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 12:52:50 +0530

    created index

---
+++ src/components/Craft/Index.jsx
@@ -0,0 +1,16 @@
+
+
+function Cart() {
+  return (
+    <div className="craft w-full flex gap-10 px-10">
+        <div className="ltext">
+
+        </div>
+        <div className="cards ">
+
+        </div>
+    </div>
+  )
+}
+
+export default Cart
