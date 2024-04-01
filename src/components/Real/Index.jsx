af8ef3d479a7a5824583b1fd723a351c9f6c0e6e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 21:56:47 +0530

    added 2 slide

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -6,6 +6,7 @@ function Real() {
       <div className="cont h-[400vh] bg-green-600">
         <div className="slides w-full h-[100vh] bg-purple-600 ">
             <div className="slide"></div>
+            <div className="slide"></div>
 
         </div>
       </div>
