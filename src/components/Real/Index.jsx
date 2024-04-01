7dce965a980af9721e4b55bcd9a8f173f725553b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 22:02:52 +0530

    updated heading

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -5,7 +5,9 @@ function Real() {
     <div className="real section w-full">
       <div className="cont h-[400vh] bg-green-600 relative">
         <div className="slides w-full h-[100vh] sticky top-0 left-0 flex ">
-            <div className="slide w-full h-screen flex-shrink-0 bg-red-500"></div>
+            <div className="slide w-full h-screen flex-shrink-0 bg-red-500">
+                <h1 className="font-[SansitaReg]"></h1>
+            </div>
             <div className="slide w-full h-screen flex-shrink-0 bg-yellow-500"></div>
             <div className="slide w-full h-screen flex-shrink-0 bg-green-500"></div>
             <div className="slide w-full h-screen flex-shrink-0 bg-blue-500"></div>
