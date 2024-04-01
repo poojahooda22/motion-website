f9f0eafb961ce07f947b2dfac22ebc437776c4bd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 20:17:26 +0530

    created row

---
+++ src/components/Row.jsx
@@ -0,0 +1,42 @@
+import img1 from '../assets/images/img1.png';
+import img2 from '../assets/images/img2.png';
+import img3 from '../assets/images/img3.png';
+import img4 from '../assets/images/img4.png';
+import img5 from '../assets/images/img5.png';
+import img6 from '../assets/images/img6.png';
+import img7 from '../assets/images/img7.png';
+
+
+function Row({translateClass}) {
+
+    const items = [
+        { text: "useful", image: img7 },
+        { text: "intuitive", image: img2 },     
+        { text: "empathetic", image: img3 },     
+        { text: "useful", image: img4 },     
+        { text: "empathetic", image: img5 },
+        { text: "intuitive", image: img2 },     
+        { text: "empathetic", image: img6 },
+        { text: "useful", image: img1 },               
+    ]
+
+  return (
+    <div className={`${translateClass} row w-full flex items-center gap-8 whitespace-nowrap`}>
+        {items.map((item, index) => {
+            return (
+                <div key={index} className='elem flex items-center gap-8'>
+                    <h1  className='font-[SansitaBold] text-6xl'>{item.text}</h1>
+                    <div className='imgdiv w-[2rem] h-[2rem]'>
+                        <img 
+                            className=''
+                            src={item.image} 
+                        />
+                    </div>
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default Row
