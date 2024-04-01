af2aa4b5ffd4ba9f85c42ae44fcea4eec5a377ab
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 21:57:46 +0530

    updated width

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -3,12 +3,12 @@
 function Real() {
   return (
     <div className="real section w-full">
-      <div className="cont h-[400vh] bg-green-600">
+      <div className="cont h-[400vh] bg-green-600 relative">
         <div className="slides w-full h-[100vh] bg-purple-600 ">
-            <div className="slide"></div>
-            <div className="slide"></div>
-            <div className="slide"></div>
-            <div className="slide"></div>
+            <div className="slide w-full"></div>
+            <div className="slide w-full"></div>
+            <div className="slide w-full"></div>
+            <div className="slide w-full"></div>
         </div>
       </div>
     </div>
