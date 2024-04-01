db2eb945b5a7b2ba690a3171b7bf320347136767
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-01 12:55:47 +0530

    updated padding

--- src/components/Craft/Index.jsx
+++ src/components/Craft/Index.jsx
@@ -3,7 +3,7 @@
 function Cart() {
   return (
     <div className="craft w-full flex gap-10 px-10">
-        <div className="ltext w-1/2 h-32 bg-red-300">
+        <div className="ltext w-1/2 py-10 bg-red-300">
 
         </div>
         <div className="cards w-1/2 h-32 bg-red-700 ">
