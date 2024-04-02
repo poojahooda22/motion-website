465afba26a589e2198b2cb1797c9a64c76691fe8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-03 00:37:58 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import './App.css'
+import Capsule from './components/Capsule/Index'
 import Craft from './components/Craft/Index'
-
+import { useEffect, useRef } from 'react';
 import Home from './components/Home/Index'
-import Navbar from './components/Navbar/Index'
+
+import Para from './components/Paragraph/Index'
+import Para2 from './components/Paragraph2/Index'
 import Real from './components/Real/Index'
 import Team from './components/Team/Index'
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      const scroll = new LocomotiveScroll({
+        el: scrollRef.current,
+        smooth: true, 
+      });
+
+      return () => {
+        scroll.destroy();
+      };
+    }
+  }, []);
+
 
   return (
-    <html>
-      <body >
-        <div className='section main w-full'>
-          <Navbar/>
-          <Home />
-          <Craft />
-          <Real />
-          <Team />
-        </div>  
-      </body>
-    </html>
-    
+    <div className='section main w-full'>
+      <Home />
+      <Craft />
+      <Real />
+      <Team />
+      <Para />
+      <Para2 />
+      <Capsule />
+    </div>    
   )
 }
 
