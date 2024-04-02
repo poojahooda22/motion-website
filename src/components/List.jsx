a5c6f1301c5fe778b66476b8b6bf991ca0a118c9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 15:29:27 +0530

    updated list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -17,14 +17,16 @@ function List() {
     <div className="list-container">
       {data.map((item, index) => {
         return (
-            <div key={index} className="listelem w-full py-10 px-10  flex 
-                items-center justify-between border-b-2 border-black ">
-                <div className="flex items-center gap-10 text-5xl">
-                    <h3 className="opacity-25">0{item.key}</h3>
-                    <h1 className="">{item.title}</h1>
+            <div key={index} className="listelem w-full py-[2.5rem] px-[2rem] border-b-2 border-black relative">
+                <div className="relative flex items-center justify-between z-[3] ">
+                    <div className="left flex gap-14 text-5xl">
+                        <h3 className="opacity-25">0{item.key}</h3>
+                        <h1 className="">{item.title}</h1>
+                    </div>
+                    <h3 className="font-[Sansita] text-xl font-medium tracking-tighter">{item.role}</h3>   
                 </div>
-                <h3 className="font-[Sansita] text-xl font-semibold">{item.role}</h3>
-                </div>
+                <div className="bluelayer absolute bottom-0 left-0 z-[2] w-full h-full bg-[--salmon]"></div>
+            </div>
         )
       })}
     </div>
