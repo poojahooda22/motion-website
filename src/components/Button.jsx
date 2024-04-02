ec70e035bccced5ee3adb4730e1fc2fe84b4ff0f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 18:35:31 +0530

    updated button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({bgColor, text}) {
   return (
     <div className={`${bgColor} w-fit px-6 py-4 border-[1px] border-[--black]`}>
         <div className="masker flex items-center gap-2 overflow-hidden ">
-            <span className="text-medium capitalize tracking-tight font-medium">{text}</span>
+            <span className="font-[Sansita] text-medium capitalize tracking-tight font-semibold">{text}</span>
             <LuArrowUpRight style={{fontSize: "24px", }} /> 
         </div>         
     </div>
