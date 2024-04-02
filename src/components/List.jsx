f25a5aa6958257aa77e942692e1cf39ea3cc69c9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 16:06:38 +0530

    updated

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -28,6 +28,7 @@ function List() {
                     </div>
                     <h3 className="font-[Sansita] text-xl font-medium tracking-tighter">{item.role}</h3>   
                 </div>
+                <div></div>
                 <div className="bluelayer absolute bottom-0 left-0 z-[2] w-full h-0 bg-[--salmon]"></div>
             </div>
         )
