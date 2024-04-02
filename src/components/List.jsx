d6de00bf5ed4ff155b8c7c7c6cab9d3bf636c904
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 16:14:02 +0530

    updated list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,17 +1,25 @@
 
-
+import img1 from '../assets/images/team1.png';
+import img2 from '../assets/images/team2.png';
+import img3 from '../assets/images/team3.png';
+import img4 from '../assets/images/team4.png';
+import img5 from '../assets/images/team5.png';
+import img6 from '../assets/images/team6.png';
+import img7 from '../assets/images/team7.png';
+import img8 from '../assets/images/team8.png';
+import img9 from '../assets/images/team9.png';
 
 
 const data = [
-    {key: 1, title: "Dr. Rick McCartney", role: "CEO", image: img1},
-    {key: 2, title: "Chris Koha", role: "COO"},
-    {key: 3, title: "Caroline Nieto", role: "Chief Product Officer"},
-    {key: 4, title: "Victor Albertos", role: "CTO"},
-    {key: 5, title: "Dr. Jana Hapfelmeier", role: "Chief Innovation Officer"},
-    {key: 6, title: "Michael Robin", role: "VP of Marketing"},
-    {key: 7, title: "Adrian Rubio", role: "VP of Engineering"},
-    {key: 8, title: "Cy Serrano", role: "VP of Product"},
-    {key: 9, title: "Lenya McGrath", role: "VP of Partnerships"},
+    {key: 1, title: "Dr. Rick McCartney", role: "CEO", img: img1},
+    {key: 2, title: "Chris Koha", role: "COO", img: img2},
+    {key: 3, title: "Caroline Nieto", role: "Chief Product Officer", img: img3},
+    {key: 4, title: "Victor Albertos", role: "CTO", img: img4},
+    {key: 5, title: "Dr. Jana Hapfelmeier", role: "Chief Innovation Officer", img: img5},
+    {key: 6, title: "Michael Robin", role: "VP of Marketing", img: img6},
+    {key: 7, title: "Adrian Rubio", role: "VP of Engineering", img: img7},
+    {key: 8, title: "Cy Serrano", role: "VP of Product", img: img8},
+    {key: 9, title: "Lenya McGrath", role: "VP of Partnerships", img: img9},
 ]
 
 
