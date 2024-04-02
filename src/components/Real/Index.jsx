53643f2f821f5a3dda2f0608f2f5f43ecab04c02
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 14:16:10 +0530

    updated index

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -17,7 +17,7 @@ function Real() {
   return (
     <div className="real section w-full">
       <div className="cont h-[400vh]  relative">
-        <div className="slides w-full h-[100vh] sticky top-0 left-0 flex ">
+        <div className="slides w-full h-[100vh] sticky top-0 left-0 flex overflow-hidden ">
             {/* 1st slide */}
             <div className="slide w-full flex items-center justify-center h-screen flex-shrink-0 ">
                 <div className="text font-[SansitaReg] text-[15vh] leading-[18vh]">
