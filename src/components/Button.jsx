63ce7eed7026a56bd1c554242167c9f8457b0af7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 16:51:02 +0530

    created button

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import { LuArrowUpRight } from "react-icons/lu";
+
+function Button({bgColor, text}) {
+  return (
+    <div className={`${bgColor} w-fit px-6 py-4 border-[1px] border-[--black]`}>
+        <div className="masker flex items-center gap-2 overflow-hidden ">
+            <span className="text-medium capitalize tracking-tight font-medium">{text}</span>
+            <LuArrowUpRight style={{fontSize: "24px", }} /> 
+        </div>         
+    </div>
+  )
+}
+
+export default Button
