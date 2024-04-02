21c09f08d5416a54cda10e386b92b48a87c4db2b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 14:38:07 +0530

    updated list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,16 @@
 
+const data = [
+    {key: 1, title: "Dr. Rick McCartney", role: "CEO"},
+    {key: 2, title: "Dr. Rick McCartney", role: "COO"},
+    {key: 3, title: "Dr. Rick McCartney", role: "Chief Product Officer"},
+    {key: 4, title: "Dr. Rick McCartney", role: "CTO"},
+    {key: 5, title: "Dr. Rick McCartney", role: "Chief Innovation Officer"},
+    {key: 6, title: "Dr. Rick McCartney", role: "VP of Marketing"},
+    {key: 7, title: "Dr. Rick McCartney", role: "VP of Engineering"},
+    {key: 8, title: "Cy Serrano", role: "VP of Product"},
+    {key: 9, title: "Lenya McGrath", role: "VP of Partnerships"},
+]
+
 
 function List() {
   return (
