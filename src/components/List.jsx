65e7ad72946f15c9ea358357afb54debdac007a0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 15:07:29 +0530

    updated list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,10 +3,10 @@ const data = [
     {key: 1, title: "Dr. Rick McCartney", role: "CEO"},
     {key: 2, title: "Chris Koha", role: "COO"},
     {key: 3, title: "Caroline Nieto", role: "Chief Product Officer"},
-    {key: 4, title: "Dr. Rick McCartney", role: "CTO"},
-    {key: 5, title: "Dr. Rick McCartney", role: "Chief Innovation Officer"},
-    {key: 6, title: "Dr. Rick McCartney", role: "VP of Marketing"},
-    {key: 7, title: "Dr. Rick McCartney", role: "VP of Engineering"},
+    {key: 4, title: "Victor Albertos", role: "CTO"},
+    {key: 5, title: "Dr. Jana Hapfelmeier", role: "Chief Innovation Officer"},
+    {key: 6, title: "Michael Robin", role: "VP of Marketing"},
+    {key: 7, title: "Adrian Rubio", role: "VP of Engineering"},
     {key: 8, title: "Cy Serrano", role: "VP of Product"},
     {key: 9, title: "Lenya McGrath", role: "VP of Partnerships"},
 ]
@@ -17,13 +17,13 @@ function List() {
     <div className="list-container">
       {data.map((item, index) => {
         return (
-            <div key={index} className="listelem w-full py-5 px-5  flex 
+            <div key={index} className="listelem w-full py-10 px-10  flex 
                 items-center justify-between border-b-2 border-black ">
                 <div className="flex items-center gap-10 text-5xl">
-                    <h3 className="opacity-25">{item.key}</h3>
+                    <h3 className="opacity-25">0{item.key}</h3>
                     <h1 className="">{item.title}</h1>
                 </div>
-                <h3 className="text-xl font-semibold">{item.role}</h3>
+                <h3 className="font-[Sansita] text-xl font-semibold">{item.role}</h3>
                 </div>
         )
       })}
