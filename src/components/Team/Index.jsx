86d82b2a5d75babcb0953623743d2c0dde02bf58
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 14:27:36 +0530

    updated index

--- src/components/Team/Index.jsx
+++ src/components/Team/Index.jsx
@@ -8,15 +8,14 @@ function Team() {
       </h1>
       <div className="list mt-10 w-full px-10">
         {/* //single list */}
-        <div className="listelem w-full py-5 bg-red-600">
-            <div>
+        <div className="listelem w-full py-5 bg-red-600 flex ">
+            <div className="flex">
                 <h3>01</h3>
                 <h1>Dr. Ric McCartney</h1>
             </div>
             <h3>CEO</h3>
 
         </div>
-
       </div>
     </div>
   )
