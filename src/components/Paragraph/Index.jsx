57755740d966547e49f8a28669f745f3b085df35
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-03 00:39:21 +0530

    updated index

--- src/components/Paragraph/Index.jsx
+++ src/components/Paragraph/Index.jsx
@@ -1,9 +1,34 @@
+import img1 from '../../assets/images/review1.jpeg';
 
 
 function Para() {
   return (
-    <div className="para section w-full h-[70vh] ">
-      
+    <div className="para section w-full flex items-center justify-center  ">
+        <div className="text w-[80%] flex flex-col items-start justify-center ">
+            <div className='w-[50%] flex items-center justify-center mb-12'>
+                <hr className='bg-zinc-400 w-[20%] h-[1.5px]' />
+            </div>    
+            <h3 className='w-[50%] font-[Sansita] text-2xl font-medium text-center leading-[2.4rem] mb-10'>
+                Working with the Significo team has been such
+                a pleasure! We took on a significant project to
+                rebuild our entire platform and the team
+                approached the project with our best interests
+                in mind. They continue to prioritize the end
+                user experience and offer amazing expertise in
+                all of the areas we lack internally. I would
+                personally be lost without this team, their
+                ability to problem solve, their openness to
+                feedback and desire to build the product like it
+                is their own.
+            </h3>
+            <div className="pers w-[50%] flex flex-col items-center justify-center gap-4">
+                <div className="image w-24 h-24 overflow-hidden rounded-full ">
+                    <img src={img1} />
+                </div>
+                <h1 className='text-2xl font-medium'>Miranda Ernst</h1>
+                <h3 className='text-zinc-500 text-xl'>Product Manager @ HealthCheck360</h3>
+            </div>
+        </div>  
     </div>
   )
 }
