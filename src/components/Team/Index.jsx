7592ec1e97f6c6e2b1ff35a72553184d9c4863d4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-02 14:26:29 +0530

    updated index

--- src/components/Team/Index.jsx
+++ src/components/Team/Index.jsx
@@ -3,9 +3,21 @@
 function Team() {
   return (
     <div className='team section font-[SansitaReg] py-20'>
-      <h1 className=" ">
+      <h1 className=" text-6xl text-center tracking-tight">
         Our Team
       </h1>
+      <div className="list mt-10 w-full px-10">
+        {/* //single list */}
+        <div className="listelem w-full py-5 bg-red-600">
+            <div>
+                <h3>01</h3>
+                <h1>Dr. Ric McCartney</h1>
+            </div>
+            
+
+        </div>
+
+      </div>
     </div>
   )
 }
