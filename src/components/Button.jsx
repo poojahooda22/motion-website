dbe62525cef6e341fd39e9412bf8278a8c548a8a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-04 15:19:04 +0530

    updated button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,22 @@
 import { LuArrowUpRight } from "react-icons/lu";
+import { gsap } from "gsap";
+import { Power2, Power4 } from 'gsap/gsap-core';
+import { useGSAP } from '@gsap/react';
 
 
 
 function Button({bgColor, text}) {
+
+  useGSAP(() => {
+    gsap.to(".masker", {
+      yPercent: 0,
+      ease: Power2.easeOut,
+    })
+  })
   return (
     <div className={`${bgColor} w-fit px-6 py-4 border-[1px] border-[--black]`}>
-        <div className="masker flex items-center gap-2 overflow-hidden ">
-            <span className="font-[Sansita] text-medium capitalize tracking-tight font-semibold text-black">{text}</span>
+        <div className="masker flex items-center gap-2 overflow-hidden  ">
+            <span className="font-[Sansita] text-medium capitalize tracking-tight font-semibold translate-y-1/2 ">{text}</span>
             <LuArrowUpRight style={{fontSize: "24px", color: "black" }} /> 
         </div>         
     </div>
