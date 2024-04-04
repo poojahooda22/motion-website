dcfbfecfa300544ffddd1c541f4bda644395190c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-04 23:29:30 +0530

    add

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -2,10 +2,15 @@
 function Footer() {
   return (
     <div className="section w-full mt-30">
-      <div className="w-full h-[30vh] bg-red-500">
+      <div className="topfoot w-full h-[30vh] bg-red-500">
+        <div className="left ">
+          <div>
+            <h1>Discover Significo.</h1>
+          </div>
+        </div>
 
       </div>
-      <div className=" w-full h-[50vh] bg-yellow-400">
+      <div className="downfoot w-full h-[50vh] bg-yellow-400">
 
       </div>
     </div>
