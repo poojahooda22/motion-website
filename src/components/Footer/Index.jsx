74abddd6b3bf49cdef67dea9d27d8b3dd8623d16
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-04 23:30:52 +0530

    add

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -3,8 +3,8 @@ function Footer() {
   return (
     <div className="section w-full mt-30">
       <div className="topfoot w-full h-[30vh] bg-red-500">
-        <div className="left ">
-          <div className="font-[SansitaReg]">
+        <div className="left w-1/3 ">
+          <div className="font-[SansitaReg] text-[4rem]">
             <h1>Discover Significo.</h1>
           </div>
         </div>
