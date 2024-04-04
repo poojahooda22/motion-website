a3a901e4ff41698b9f5bb66ff538446e0f7352b0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-04 23:27:32 +0530

    updated footer

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -2,7 +2,7 @@
 function Footer() {
   return (
     <div className="section w-full mt-30">
-      <div className="w-1/3 h-[30vh] bg-red-500">
+      <div className="w-full h-[30vh] bg-red-500">
 
       </div>
       <div className=" w-1/2 h-[50vh] bg-yellow-400">
