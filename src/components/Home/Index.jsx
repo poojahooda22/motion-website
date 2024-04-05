a4fa24d8d17ffd330098e1737ebb61e39d3caf4d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-05 21:16:15 +0530

    updated index

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -163,7 +163,7 @@ function Home() {
             >
                 {/* /* top Heading div */ }
                 <div className=' heading absolute top-[7%] left-1/2 -translate-x-1/2 w-72'>
-                    <h2 className='toptext text-[3vh] font-[Sansita] font-medium text-center'>Crafting a new paradigm of healthcare, one that is</h2>
+                    <h2 className='toptext text-[2.4vh] font-[Sansita] font-medium text-center'>Crafting a new paradigm of healthcare, one that is</h2>
                 </div>
 
                 <div 
