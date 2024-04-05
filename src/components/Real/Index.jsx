319c4d21268fb1b9f7b059abb90fc111ab2e9613
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-05 19:59:33 +0530

    updated

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -82,8 +82,8 @@ function Real() {
                 </div>
                 <div className='w-[60%] text-center font-[SansitaReg] relative'>
                     <h3 className='font-[Sansita] w-1/3 text-left font-semibold tracking-tight text-xl absolute top-0 left-0 z-[3] -translate-y-1/2 -translate-x-1/3'>We’re on a mission to impact as many lives as possible and build a better company while we do it. Here’s our progress.</h3>
-                    <h1 className=' font-semibold text-[9rem] leading-none text-white '>20.4M</h1>
-                    <h3 className='text-[2.4rem] font-semibold leading-[3rem]'>Real people — real lives — we have built products and solutions for.</h3>    
+                    <h1 className=' font-semibold text-[15vh] leading-none text-white '>20.4M</h1>
+                    <h3 className='text-[5vh] font-semibold leading-[5vh]'>Real people — real lives — we have built products and solutions for.</h3>    
                 </div>
                 <div 
                         className="image absolute w-[10rem] h-[10rem] 
