a353556eb3217b7ca232b592df231983a75cc83c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-05 22:33:16 +0530

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
