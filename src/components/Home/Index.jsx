9adf1a32f9377959421c6502bebec6062906e5a7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-06 23:19:07 +0530

    add

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -188,7 +188,8 @@ function Home() {
                 relative overflow-hidden "
             >
                 {/* /* top Heading div */ }
-                <div className=' heading absolute top-[7%] left-1/2 -translate-x-1/2 w-72'>
+                <div 
+                className=' heading absolute top-[7%] left-1/2 -translate-x-1/2 w-72'>
                     <h2 className='toptext text-[2.4vh] font-[Sansita] font-medium text-center'>Crafting a new paradigm of healthcare, one that is</h2>
                 </div>
 
