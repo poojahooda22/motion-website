c997b14d2a8c5a7d5e5adf7d282e7fda62448d8c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-06 23:12:08 +0530

    add

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -205,7 +205,7 @@ function Home() {
                         <Row 
                             translateClass="-translate-x-2/3"  
                             direction="rgt"
-                            />
+                        />
                         <Row translateClass="-translate-x-1/4"  direction="lft" />
                         <Row translateClass="-translate-x-1/3"  direction="rgt"/>
                     </div>            
