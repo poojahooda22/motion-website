35c9af55601623cebd8056c94a1f9b945e1d147f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-06 22:40:47 +0530

    add

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -66,7 +66,8 @@ function Footer() {
                 the latest updates.
             </h1>
             <div className='flex items-center gap-6 '>
-              <input type="text" 
+              <input 
+              type="text" 
               placeholder="Enter your email address" className="bg-[--light] w-[350px] h-[5.8vh] px-4 text-[2vh] "/>
               <Button text="SUBSCRIBE" bgColor="bg-[#f5f19c]" />
             </div>
