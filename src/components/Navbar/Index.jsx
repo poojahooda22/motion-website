b9cfe17fc8a646fe8aeb47e798d91c72f902f94d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-07 21:17:19 +0530

    add

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -34,7 +34,8 @@ function Navbar() {
             className="flex items-center 
             justify-between"
         >
-            <div className="logo w-[20vh] h-[10vh] cursor-pointer z-[9]">
+            <div 
+            className="logo w-[20vh] h-[10vh] cursor-pointer z-[9]">
                 {/* logo */}
                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 141 41" fill="none">
                     <g >
