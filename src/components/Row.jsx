1dabe0de35ef827182b7999b7a3395811c6cd75a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-08 22:17:17 +0530

    added

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -36,7 +36,8 @@ function Row({ translateClass, direction }) {
         {items.map((item, index) => {
             return (
                 <div key={index} className='elem flex items-center gap-8'>
-                    <h1  className='font-[SansitaBold] text-[6vh] sm:text-[8.4vh] 
+                    <h1 
+                    className='font-[SansitaBold] text-[6vh] sm:text-[8.4vh] 
                     leading-[6vh] sm:leading-[9vh]'>
                         {item.text}
                     </h1>
