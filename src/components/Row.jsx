9aa30e3d1b06f776021e551899822c632b422931
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-08 22:18:22 +0530

    add

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -32,7 +32,7 @@ function Row({ translateClass, direction }) {
 
   return (
     <div className={`${translateClass} ${direction} row w-full flex items-center  gap-8 
-    whitespace-nowrap mb-2`}>
+        whitespace-nowrap mb-2`}>
         {items.map((item, index) => {
             return (
                 <div 
