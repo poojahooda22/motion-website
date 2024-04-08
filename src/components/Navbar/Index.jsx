7a05dbc95ad4ac7c58d4088858495f746ae5b196
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-08 22:06:21 +0530

    add

--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -63,7 +63,8 @@ function Navbar() {
                 {["Solutions", "About", "Insight", "Team", "Careers"].map((item, index) => (
                     <h4 
                         key={index} 
-                        className={`${styles.links} relative py[2.4vh] px-[2.2vh] text-center  
+                        className={`${styles.links} relative py[2.4vh] px-[2.2vh] 
+                        text-center  
                         font-[Sansita]text-[1vw] overflow-hidden`}> 
                         <a className={`${styles.atag} relative`}>{item} </a>                    
                     </h4>   
