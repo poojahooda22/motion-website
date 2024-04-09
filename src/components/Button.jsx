b4b13c3a4f3ec8550159a3cbd4c50915b3c5378d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-09 20:09:26 +0530

    add

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -15,7 +15,7 @@ function Button({bgColor, text}) {
         relative cursor-pointer`}
       >
         <span 
-        className={`${styles.spanMask} font-[Sansita] text-[1.8vh] 
+          className={`${styles.spanMask} font-[Sansita] text-[1.8vh] 
         capitalize tracking-normal
             font-semibold `}>{text}</span>
             <LuArrowUpRight style={{fontSize: "24px", color: "black", }} className={`${styles.iconMask}`} /> 
