fde219201f9ede3d79c5180c6fc1f5fe2bb6218b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-09 20:04:27 +0530

    updated index

--- src/components/Team/Index.jsx
+++ src/components/Team/Index.jsx
@@ -8,11 +8,11 @@ function Team() {
   return (
     <div  data-color="white" className='team section font-[SansitaReg] py-20'>
       <div className="head1">
-        <h1 className=" text-6xl text-center tracking-tight">
+        <h1 className="text-5xl sm:text-6xl text-center tracking-tight">
           Our Team
         </h1>
       </div>
-      <div className="list mt-10 w-full px-10">
+      <div className="list mt-10 w-full px-8">
         {/* //single list */}
         <List />
         <div className='flex items-center justify-center py-20'>
