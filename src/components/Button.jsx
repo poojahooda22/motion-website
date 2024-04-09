57210293b12d6e152792c3782dbaee6de09e566d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-09 20:08:30 +0530

    add

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,7 +10,8 @@ function Button({bgColor, text}) {
       className={`${bgColor} section w-fit sm:w-fit px-4 
       py-[1.6vh] border-[1px] border-[--black]`}
     >
-      <div className= {`${styles.masker} flex items-center gap-2 overflow-hidden relative cursor-pointer`}>
+      <div 
+      className= {`${styles.masker} flex items-center gap-2 overflow-hidden relative cursor-pointer`}>
             <span className={`${styles.spanMask} font-[Sansita] text-[1.8vh] capitalize tracking-normal
             font-semibold `}>{text}</span>
             <LuArrowUpRight style={{fontSize: "24px", color: "black", }} className={`${styles.iconMask}`} /> 
