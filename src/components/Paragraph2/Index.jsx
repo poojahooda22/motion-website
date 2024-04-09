5f15bd904df91a6ab3bd5ab51fdda778d3c5b115
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-09 20:04:11 +0530

    updated index

--- src/components/Paragraph2/Index.jsx
+++ src/components/Paragraph2/Index.jsx
@@ -30,18 +30,19 @@ function Para2() {
         })
     },[]);
   return (
-    <div data-color="white" className="para2 section w-full flex items-center justify-center -translate-y-1/5 ">
-        <div className="text w-[80%] flex flex-col items-end justify-center ">
-            <div className='w-[40%] flex items-center justify-center mb-12'>
+    <div data-color="white" className="para2 section w-full flex items-center mt-32 px-8
+    justify-center -translate-y-1/5 ">
+        <div className="text sm:w-[80%] flex flex-col items-center sm:items-end justify-center ">
+            <div className='hidden w-[40%] sm:flex items-center justify-center mb-12'>
                 <hr className='bg-zinc-400 w-[20%] h-[.3vh]' />
             </div>    
-            <h3 className='textpara2 w-[40%] text-purple-600 font-[Sansita] text-[3.5vh] font-medium text-center leading-[5vh] mb-10'> Working with Significo and their recommendation service has been a game-changer for our occupational prevention efforts. Our customers are extremely satisfied with the tool&apos;s impressive technical capabilities and data-driven approach. But what truly sets it apart is the intuitive and modern user experience it offers, making it a breeze for our clients to navigate. By leveraging this service, our customers have successfully tackled presenteeism and absenteeism, making a significant impact on their workforce&apos;s well-being.</h3>
-            <div className="pers w-[40%] flex flex-col items-center justify-center gap-4">
+            <h3 className='textpara2 sm:w-[40%] text-purple-600 font-[Sansita] text-[2.4vh] sm:text-[3.5vh] font-medium text-center leading-[5vh] mb-10'> Working with Significo and their recommendation service has been a game-changer for our occupational prevention efforts. Our customers are extremely satisfied with the tool&apos;s impressive technical capabilities and data-driven approach. But what truly sets it apart is the intuitive and modern user experience it offers, making it a breeze for our clients to navigate. By leveraging this service, our customers have successfully tackled presenteeism and absenteeism, making a significant impact on their workforce&apos;s well-being.</h3>
+            <div className="pers w-[40%] flex flex-col items-center justify-center gap-2">
                 <div className="image w-24 h-24 overflow-hidden rounded-full ">
                     <img src={img1} />
                 </div>
-                <h1 className='text-[3.8vh] font-medium'>Emek Altun</h1>
-                <h3 className='text-zinc-500 text-[2.4vh]'>CEO @ Vitaservices</h3>
+                <h1 className='text-[2.8vh] sm:text-[3.8vh] font-medium'>Emek Altun</h1>
+                <h3 className='text-zinc-500 text-[2.4vh] whitespace-nowrap'>CEO @ Vitaservices</h3>
             </div>
         </div>  
     </div>
