6913c2273419c8d34ba627b3aad098929e88d6a1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-09 19:15:17 +0530

    updated index

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -5,14 +5,14 @@ import Button from  '../Button'
 
 function Footer() {
   return (
-    <div className="section w-full mt-30 ">
-      <div className="topfoot  bg-[#f5f19c] pt-16 pb-16 flex items-center justify-between">
-        <div className="left w-1/2 px-10  ">
-          <div className=" font-[SansitaReg] text-[4.2rem] w-1/3 leading-[5rem] mb-10 ">
+    <div className="section w-full sm:mt-30 ">
+      <div className="topfoot  bg-[#f5f19c] pt-16 pb-16 sm:flex items-center justify-between">
+        <div className="left sm:w-1/2 px-8 sm:px-8  ">
+          <div className="first order-1 font-[SansitaReg]  text-[6vh] leading-[7vh] sm:text-[4.2rem] w-1/3 sm:leading-[5rem] sm:mb-10 ">
             <h1>Discover Significo.</h1>
           </div>
           {/* <Button text="Let's Go" bgColor="bg-[#f5f19c]" /> */}
-          <div className='w-fit px-4 py-[1.6vh] bg-[--black] text-[#f5f19c]'>
+          <div className='middle order-3 sm:order-2 w-[20vh] text-center mt-6 px-4 py-4 sm:w-fit sm:px-4 sm:py-[1.6vh] bg-[--black] text-[#f5f19c] whitespace-nowrap'>
             <div 
               className= {`${styles.masker} flex items-center gap-2 overflow-hidden 
               relative cursor-pointer`}>
@@ -23,15 +23,15 @@ function Footer() {
           </div>
         </div>
 
-        <div className="right flex w-1/2 items-center justify-between px-10">
-          <div className="rght1 w-2/3 flex flex-col items-end ">
-            <h3 className="text-[1.4rem] font-[Sansita] font-medium">
-              Find out how Significo’s people-centered<br/>
-              designs can make health technology more<br/>
+        <div className="right order-2 sm:order-3  flex sm:w-1/2 items-center justify-between sm:px-10">
+          <div className="rght1 sm:w-2/3 flex flex-col items-end px-8 mt-6">
+            <h3 className="text-[2.4vh] sm:text-[1.4rem] font-[Sansita] font-medium">
+              Find out how Significo’s people-centered
+              designs can make health technology more
               empowering.
             </h3>
           </div>
-          <div className="rght2 relative w-[150px] h-[150px]  overflow: hidden;">
+          <div className="hidden sm:inline-block rght2 relative w-[150px] h-[150px]  overflow: hidden;">
             <div className={`${styles.loopFoot} `}></div>
             <div className="icon w-[120px] h-[120px] rounded-full border-black border-[1px] px-10 py-10 absolute top-1/2 left-1/2  -translate-y-1/2 -translate-x-1/2 overflow-hidden">
               <div className="w-10 h-10 absolute top-1/2 left-1/2  -translate-y-1/2 -translate-x-1/2">
@@ -47,48 +47,42 @@ function Footer() {
       </div>
 
 
-      <div className="downfoot w-full h-fit bg-[--black] px-10 py-32">
-        <div className='w-full flex items-start justify-between'>
-          <div className='first'>
+      <div className="downfoot w-full h-fit bg-[var(--black)] px-8 py-10 sm:py-32">
+        <div className='flex sm:flex-row sm:items-start justify-between space-y-4 sm:space-y-0 sm:space-x-4'>
+          <div className=' hidden sm:inline-block '>
             {["Home", "Solutions", "About", "Team", "Contact"].map((item, index) => {
               return (
                 <div key={index} className='pb-4'>
                   <h3 className='font-[SansitaReg] text-[2.5vh] text-white font-semibold'>
                     {item}
-                </h3>  
+                  </h3>  
                 </div>
               ) 
             })}
           </div>
-          <div className='middle w-1/3'>
-            <h1 className='text-white w-2/3 pb-10 font-[SansitaReg] 
-            text-[2.8vh] font-semibold'>Join our mailing list for
-                the latest updates.
-            </h1>
-            <div className='flex items-center gap-6 '>
+          <div className=''>
+            <h1 className='text-white pb-10 font-[SansitaReg] text-[2.8vh] font-semibold'>Join our mailing list for the latest updates.</h1>
+            <div className='flex items-center gap-6'>
               <input 
                 type="text" 
                 placeholder="Enter your email address" 
-                className="bg-[--light] w-[350px] h-[5.8vh] px-4 text-[2vh] "
+                className="bg-[var(--light)] w-full sm:w-[350px] h-[5.9vh] px-4 text-[2vh]"
               />
               <Button text="SUBSCRIBE" bgColor="bg-[#f5f19c]" />
             </div>
           </div>
-          <div className='last'>
+          <div className=' hidden sm:inline-block '>
             {["Insights", "Newsroom", "Resources", "Careers", "Contact"].map((item, index) => {
               return (
                 <div key={index} className='pb-4'>
                   <h3 className='font-[SansitaReg] text-[2.5vh] text-white font-semibold'>
                     {item}
-                </h3>  
+                  </h3>  
                 </div>
               ) 
             })}
-          </div>
-          
+          </div>  
         </div>
-
-        <div></div>
       </div>
     </div>
   )
