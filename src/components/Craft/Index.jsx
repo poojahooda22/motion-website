a3cdd6ba687c406ad0471212ce5e9a5f3333aa81
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-09 19:15:08 +0530

    updated index

--- src/components/Craft/Index.jsx
+++ src/components/Craft/Index.jsx
@@ -62,21 +62,43 @@ function Craft() {
             stagger: 0.5
             }
         });
-        tl.fromTo(
-            ".card", 
-            {                
-                y: 600,
-                scale: .8,       
-            },
-            {
-                opacity: 1,
-                scale: 1,
+        if (window.innerWidth < 768) {
+            // Mobile Animation (X-axis)
+            tl.fromTo('.card', {
+                x: 0, // Starting from left (negative value)
+            }, {
 
+                x: -100,
+                ease: Power4
+            });
+        } else {
+            // Desktop Animation (Y-axis)
+            tl.fromTo('.card', {
+                y: 700,
+                scale: .9,
+            }, {
                 y: 0,
+                scale: 1.1,
                 ease: Power4,
-                transformOrigin: "50% 50%",
-            }
-        );                
+                transformOrigin: "bottom 50% -50",
+            });
+        }
+        // tl.fromTo(
+        //     ".card", 
+        //     {                
+        //         y: 400,
+        //         scale: .8,
+        //         gap: 10,       
+        //     },
+        //     {
+        //         opacity: 1,
+        //         scale: 1,
+
+        //         y: 0,
+        //         ease: Power4,
+        //         transformOrigin: "bottom 50% -50",
+        //     }
+        // );                
     }, container );
 
    
@@ -84,26 +106,26 @@ function Craft() {
     <div 
         data-color="cyan"  
         ref={container} 
-        className="craft section w-full flex gap-10  
-        justify-between items-center px-6 py-8 sm:px-10 relative"
+        className="craft section w-full sm:flex gap-x-40 justify-between 
+          items-center px-8 py-8 sm:px-10 relative "
     >
-        <div className="ltext sticky top-[10%] left-0  ">
+        <div className="ltext sm:sticky sm:top-[10%] left-0 sm:w-1/2 ">
             <p 
-                className="ptag font-[Sansita] text-[2.9vh] 
-                font-medium leading-[4.2vh] "
+                className="ptag font-[Sansita]  text-[2.6vh] sm:text-[2.9vh] 
+                font-medium leading-[4.4vh] sm:leading-[4.2vh] "
             >
-                Significo is a custom health software developer founded on the belief<br/>
-                that technology can transform healthcare to put people first. We put<br/>
-                humanity back at the center of healthcare by simplifying complexity,<br/>
+                Significo is a custom health software developer founded on the belief
+                that technology can transform healthcare to put people first. We put
+                humanity back at the center of healthcare by simplifying complexity,
                 accelerating capacity, and improving outcomes.
             </p>
-                <h1 className="texthead font-[SansitaReg] text-[10vh] leading-[12vh] 
+                <h1 className="texthead font-[SansitaReg] text-[6vh] leading-[7vh] sm:text-[10vh] sm:leading-[12vh] 
                 mt-10 mb-10">We Craft<br/> Human-Centric <br/> Health Software</h1>
             {/* button */}
             <Button  bgColor="bg-none" text="OUR SOLUTIONS" />
         </div>
         <div
-            className="right cards w-full sm:w-1/2 ">                
+            className="right cards sm:w-1/2  ">                
             <Card />
         </div>
     </div>
@@ -111,3 +133,37 @@ function Craft() {
 }
 
 export default Craft
+
+
+
+// return (
+//     <div 
+//         data-color="cyan"  
+//         ref={container} 
+//         className="craft section w-full flex gap-10  
+//         justify-between items-center px-6 py-8 sm:px-10 relative"
+//     >
+//         <div className="ltext sticky top-[10%] left-0  ">
+//             <p 
+//                 className="ptag font-[Sansita] text-[2.9vh] 
+//                 font-medium leading-[4.2vh] "
+//             >
+//                 Significo is a custom health software developer founded on the belief<br/>
+//                 that technology can transform healthcare to put people first. We put<br/>
+//                 humanity back at the center of healthcare by simplifying complexity,<br/>
+//                 accelerating capacity, and improving outcomes.
+//             </p>
+//                 <h1 className="texthead font-[SansitaReg] text-[10vh] leading-[12vh] 
+//                 mt-10 mb-10">We Craft<br/> Human-Centric <br/> Health Software</h1>
+//             {/* button */}
+//             <Button  bgColor="bg-none" text="OUR SOLUTIONS" />
+//         </div>
+//         <div
+//             className="right cards w-full sm:w-1/2 ">                
+//             <Card />
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Craft
