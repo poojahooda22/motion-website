e64572887e8c35e4bb41899174cf9ead0f85b12e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-10 19:57:11 +0530

    add

--- src/components/Craft/Index.jsx
+++ src/components/Craft/Index.jsx
@@ -95,7 +95,7 @@ function Craft() {
 
         //         y: 0,
         //         ease: Power4,
-        //         transformOrigin: "bottom ,
+        //         transformOrigin: 
         //     }
         // );                
     }, container );
