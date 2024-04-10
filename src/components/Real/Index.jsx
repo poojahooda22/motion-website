bab535ca391ca4a46d1d2499fe8e9ff3fc1cf404
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-10 16:07:06 +0530

    updated index

--- src/components/Real/Index.jsx
+++ src/components/Real/Index.jsx
@@ -57,7 +57,7 @@ function Real() {
         >
             {/* 1st slide */}
             <div 
-                className="slide w-full sm:flex items-center 
+                className="slide w-full flex sm:flex items-center 
                 justify-center h-screen flex-shrink-0 "
             >
                 
@@ -73,7 +73,7 @@ function Real() {
                     </h1>
                 </div>
                 <div 
-                    className="image absolute w-[25vh] h-[25vh] sm:w-[45vh] sm:h-[45vh] -translate-y-2/5
+                    className="image absolute w-[20vh] h-[20vh] sm:w-[45vh] sm:h-[45vh] -translate-y-2/5
                     top-1/2 sm:-translate-y-1/2 translate-x-1/2
                     right-0 "
                 >
@@ -86,7 +86,7 @@ function Real() {
             </div>
             {/* 2nd slide */}
             <div   
-                className="slide w-full h-screen flex items-center 
+                className="slide w-full h-screen sm:flex flex items-center 
                 justify-center flex-shrink-0 relative "
             >
                 <div 
@@ -101,8 +101,8 @@ function Real() {
                 <div className='w-[60%] text-center font-[SansitaReg] relative'>
                     <h3 
                         className='font-[Sansita] sm:w-1/3 text-left font-semibold 
-                        tracking-tight text-[2.2vh] sm:absolute top-0 left-0 z-[3] 
-                        -translate-y-1/2 -translate-x-1/3'
+                        tracking-tight text-[2.2vh] sm:absolute top-0 left-0 z-[3]  -translate-y-2/3 
+                        sm:-translate-y-1/2 -translate-x-1/3'
                     >
                         We’re on a mission to impact as many lives as possible and build a better company while we do it. Here’s our progress.
                     </h3>
@@ -114,7 +114,7 @@ function Real() {
                     </h3>    
                 </div>
                 <div 
-                    className="image absolute w-[15vh] sm:w-[20vh] sm:h-[20vh] top-4/5
+                    className="image absolute w-[12vh] sm:w-[20vh] sm:h-[20vh] top-4/5 bottom-[10vh]
                     sm:top-2/3 sm:left-1/4 -translate-x-1/2"
                 >
                     <img 
@@ -123,7 +123,8 @@ function Real() {
                     />
                 </div>
                 <div 
-                    className="image absolute w-[20vh] sm:w-[45vh] sm:h-[45vh] first-letter: sm:bottom-0 right-1/6 
+                    className="image absolute w-[20vh] sm:w-[45vh] sm:h-[45vh] first-letter: 
+                    sm:bottom-0 sm:right-1/6 right-0
                     translate-y-[20vh]"
                 >
                     <img 
@@ -146,7 +147,7 @@ function Real() {
             
             {/* 3rd slide */}
             <div 
-                className="slide w-full h-screen flex items-center justify-center 
+                className="slide w-full h-screen flex sm:flex items-center justify-center 
                 flex-shrink-0  relative"
             >
                 <div 
@@ -198,7 +199,7 @@ function Real() {
 
             {/* 4th slide */}
             <div 
-                className="slide w-full h-screen  flex items-center justify-center 
+                className="slide w-full h-screen flex  sm:flex items-center justify-center 
                 relative flex-shrink-0 "
             >
                 <div 
@@ -214,7 +215,7 @@ function Real() {
                     <h1 className=' font-semibold text-[10vh] sm:text-[20vh] leading-none text-white'>
                         13
                     </h1>
-                    <h3 className='text-[3.5vh] sm:text-[6vh] font-semibold sm:leading-[8vh]'>
+                    <h3 className='text-[3.1vh] leading-[3.4vh] sm:text-[6vh] font-semibold sm:leading-[8vh]'>
                         Nationalities Represented on Our <br/> Team.
                     </h3>    
                 </div>
