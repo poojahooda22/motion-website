9ca15b79939feb01eff6481cdee78b6b5c79dfe4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-10 19:59:33 +0530

    add

--- src/components/Craft/Index.jsx
+++ src/components/Craft/Index.jsx
@@ -90,7 +90,7 @@ function Craft() {
         //         gap: 10,       
         //     },
         //     {
-        //         opacity: 1,
+        //         opacity
         // );                
     }, container );
 
