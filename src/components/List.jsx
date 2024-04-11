24f9c9023b939f775a3c21c330b1c2145dbcae72
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-11 20:15:49 +0530

    added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -75,7 +75,8 @@ function List() {
               flex justify-between items-center
               border-b-2 border-black sm:relative"
             >
-              <div className="relative w-full sm:flex sm:items-center justify-between z-[3] ">
+              <div className="relative w-full sm:flex sm:items-center justify-between z-[3] "
+              >
                     <div className="left sm:flex items-center gap-14 sm:text-5xl">
                         <h3 className="hidden sm:inline-block opacity-25">0{item.key}</h3>
                         <h1 className="text-blue-600 text-3xl sm:text-black sm:text-[6vh]">{item.title}</h1>
