6fb4eb5bad8db055623f74866c4251f3ab04e45c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-04-11 20:24:22 +0530

    added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -89,7 +89,7 @@ function List() {
                 {item.role}
               </h3>   
             </div>
-              <div 
+            <div 
                   className='picture w-[14vh] h-[14vh] opacity-100 right-0 sm:opacity-0 sm:absolute z-[4] sm:top-1/2 
                   sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:w-[15rem] sm:h-[15rem] 
                   overflow-hidden rounded-full'
