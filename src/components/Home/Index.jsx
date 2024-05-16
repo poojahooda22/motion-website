7f73b6d05a9c7bef1e6d6fbf6254fce7c3053f67
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-16 18:37:15 +0530

    updated

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -187,7 +187,7 @@ function Home() {
 
                 <div 
                     
-                    className='slidesm absolute scale-[1]  sm:scale-100 top-1/2 left-1/2
+                    className='slidesm absolute scale-[5]  top-1/2 left-1/2
                     -translate-x-1/2 -translate-y-1/2 w-[90%]'
                 >    
                     <div className='row'>
